3dd93eca8dc779c10b4a440ef811d9e0c53f5080
piiyuushh <piyushkarn76@gmail.com>
2024-12-26 15:00:41 +0545

    added model

---
+++ src/com/bakeease/model/adminModel.java
@@ -0,0 +1,41 @@
+package com.bakeease.model;
+
+public class adminModel {
+    private String productName;
+    private String quantity;
+    private String price;
+    private String weight;
+
+    // Getters and setters for global variables
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+}
